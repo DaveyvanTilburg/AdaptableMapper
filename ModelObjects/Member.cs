55f6481e5b1af3110bacf8ae6f23c51541ba4940
Davey van Tilburg <davey@smarthotel.nl>
2019-10-30 09:14:09 +0100

    Readd with correct cap

---
+++ ModelObjects/Member.cs
@@ -0,0 +1,11 @@
+﻿using AdaptableMapper.Model.Language;
+using System.Collections.Generic;
+
+namespace Hercules.ModelObjects
+{
+    public class Member : ModelBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<CrewMember> CrewMembers { get; set; } = new List<CrewMember>();
+    }
+}
