49a73f9be35f75707f89e5e9c3ab7928d4f4276f
Davey van Tilburg <davey@smarthotel.nl>
2019-12-03 15:53:16 +0100

    Add listOfValueMutations Add ReplaceMutation

---
+++ AdaptableMapper/ValueMutations/ListOfValueMutations.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdaptableMapper.ValueMutations
+{
+    public class ListOfValueMutations : ValueMutation
+    {
+        private List<ValueMutation> ValueMutations { get; set; }
+
+        public string Mutate(string source)
+        {
+            var result = source;
+
+            if (!Validate())
+                return result;
+
+            foreach(ValueMutation valueMutation in ValueMutations)
+                result = valueMutation.Mutate(result);
+
+            return result;
+        }
+
+        private bool Validate()
+        {
+            bool result = true;
+
+            if ((ValueMutations?.Any() ?? false) == false)
+            {
+                Process.ProcessObservable.GetInstance().Raise("ListOfValueMutations#11; ValueMutations is empty", "error");
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdaptableMapper/ValueMutations/ReplaceMutation.cs
@@ -0,0 +1,15 @@
+﻿using AdaptableMapper.Traversals;
+
+namespace AdaptableMapper.ValueMutations
+{
+    public class ReplaceMutation : ValueMutation
+    {
+        public GetValueTraversal GetValueTraversalValue { get; set; }
+        public GetValueTraversal GetValueTraversalReplaceValue { get; set; }
+
+        public string Mutate(string source)
+        {
+            return source;
+        }
+    }
+}
