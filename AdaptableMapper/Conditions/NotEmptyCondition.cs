30dffe71571afb80cca62c1bf57623cc84636acf
Davey van Tilburg <daveyvantilburg@gmail.com>
2019-12-16 08:54:38 +0100

    Adde NotEmptyCondition

---
+++ AdaptableMapper/Conditions/NotEmptyCondition.cs
@@ -0,0 +1,23 @@
+﻿using AdaptableMapper.Configuration;
+using AdaptableMapper.Traversals;
+
+namespace AdaptableMapper.Conditions
+{
+    public sealed class NotEmptyCondition : Condition
+    {
+        public NotEmptyCondition(GetValueTraversal getValueTraversal)
+        {
+            GetValueTraversal = getValueTraversal;
+        }
+
+        public GetValueTraversal GetValueTraversal { get; set; }
+
+        public bool Validate(Context context)
+        {
+            string value = GetValueTraversal.GetValue(context);
+
+            bool result = !string.IsNullOrWhiteSpace(value);
+            return result;
+        }
+    }
+}
